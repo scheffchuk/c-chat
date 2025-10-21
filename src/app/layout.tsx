c849d5523fdd689e57ce975385e6cc58284ea695
ScheffChuk <darthusian@gmail.com>
2025-10-21 10:58:09 +0900

    feat: layout config, shadcn init, next-theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,8 +15,37 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "C Chat",
-  description: "A AI chat app built for graduation assignment, powered by Next.js and Vercel, AI SDK, AI Gateway, and Convex as the backend.",
+  description:
+    "A AI chat app built for graduation assignment, powered by Next.js and Vercel, AI SDK, AI Gateway, and Convex as the backend.",
 };
+
+export const viewport = {
+  maximumScale: 1,
+};
+
+// Maybe I don't need this
+const LIGHT_THEME_COLOR = "hsl(0 0% 100%)";
+const DARK_THEME_COLOR = "hsl(240deg 10% 3.92%)";
+const THEME_COLOR_SCRIPT = `\
+(function() {
+  var html = document.documentElement;
+  var meta = document.querySelector('meta[name="theme-color"]');
+  if (!meta) {
+    meta = document.createElement("meta");
+    meta.setAttribute("name", "theme-color");
+    document.head.appendChild(meta);
+  }
+  function updateThemeColor() {
+    var isDark = html.classList.contains("dark");
+    meta.setAttribute("content", isDark ? "${DARK_THEME_COLOR}" : "${LIGHT_THEME_COLOR}")
+    }
+    var obserer = new MutationObserver(updateThemeColor);
+    obserer.observe(html, {
+      attributes: true,
+      attributeFilter: ["class"],
+    });
+    updateThemeColor();
+})();`;
 
 export default function RootLayout({
   children,
@@ -23,11 +53,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} `}
+      suppressHydrationWarning
+    >
+      <head>
+        <script dangerouslySetInnerHTML={{ __html: THEME_COLOR_SCRIPT }} />
+      </head>
+      <body className="antialiased">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
