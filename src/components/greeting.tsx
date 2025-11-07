248e9f9ccb4aff015a50f0083b16e2604339c972
ScheffChuk <darthusian@gmail.com>
2025-11-07 11:47:18 +0900

    feat(ui): Bigger font size for greeting message

--- src/components/greeting.tsx
+++ src/components/greeting.tsx
@@ -5,12 +5,11 @@ export default function Greeting() {
   const { user } = useUser();
   return (
     <div
-      className="justiy-center mx-auto mt-4 flex size-full max-w-3xl flex-col px-4 md:mt-16 md:px-8"
+      className="justiy-center mx-auto mt-4 flex size-full max-w-3xl flex-col px-4 md:mt-16 md:px-8 font-semibold text-2xl md:text-4xl text-primary/90"
       key="overview"
     >
       <motion.div
         animate={{ opacity: 1, y: 0 }}
-        className="font-semidbold text-xl md:text-2xl"
         exit={{ opacity: 0, y: 10 }}
         initial={{ opacity: 0, y: 10 }}
         transition={{ delay: 0.5 }}
@@ -19,7 +18,6 @@ export default function Greeting() {
       </motion.div>
       <motion.div
         animate={{ opacity: 1, y: 0 }}
-        className="text-xl text-zince-500 md:text-2xl"
         exit={{ opacity: 0, y: 10 }}
         initial={{ opacity: 0, y: 10 }}
         transition={{ delay: 0.6 }}
